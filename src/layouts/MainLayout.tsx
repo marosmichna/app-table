36fc1d310afcfba004c7b51ca4b3f5b36030f37b
marosmichna <maros24121990@gmail.com>
2025-11-23 16:02:24 +0100

    Edit layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,17 +1,23 @@
 import { Outlet } from "react-router-dom";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/AppSIdebar";
+import { AnnouncementProvider } from "@/contexts/AnnouncementContext";
 
 export default function MainLayout() {
   return (
-    <SidebarProvider>
-      <div className="flex h-screen w-screen overflow-hidden">
-        <AppSidebar />
-
-        <main className="flex-1 p-6 h-screen overflow-hidden">
-          <Outlet />
-        </main>
+    <AnnouncementProvider> 
+      <div className="h-screen w-screen">
+        <SidebarProvider>
+          <div className="h-full w-full flex">
+            <AppSidebar />
+            <main className="flex-1 min-h-screen overflow-y-auto">
+              <div className="w-full max-w-6xl mx-auto p-4">
+                <Outlet />
+              </div>
+            </main>
+          </div>
+        </SidebarProvider>
       </div>
-    </SidebarProvider>
+    </AnnouncementProvider>
   );
 }
